a0580b88fce37628e00786a5fc1ff24328036c04
AvikNayak <nayakavik19@gmail.com>
2024-11-22 00:04:02 +0530

    working on sign in and sign up pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className={"flex-center h-screen"}>
+    <div className={"flex-center"}>
       <h1 className={"h1"}>
         FileFox - Store files online without any hassle for free!
       </h1>
